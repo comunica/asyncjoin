cfbf52ecb18c6fab3d11d79206d07e5bf13ced5d
Joachim Van Herwegen <joachimvh@gmail.com>
2017-11-23 13:35:33 +0100

    Add Mergeiterator

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const HashJoin = require('./join/HashJoin');
 const NestedLoopJoin = require('./join/NestedLoopJoin');
 const SymmetricHashJoin = require('./join/SymmetricHashJoin');
 
+const MergeStream = require('./util/MergeIterator');
+
 module.exports = {
-  DynamicNestedLoopJoin,
-  HashJoin,
-  NestedLoopJoin,
-  SymmetricHashJoin
+    DynamicNestedLoopJoin,
+    HashJoin,
+    NestedLoopJoin,
+    SymmetricHashJoin,
+
+    MergeStream
 };
