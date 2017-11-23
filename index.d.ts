157ab4fc6f1549468bf39b1f916fd8f9a216106d
Joachim Van Herwegen <joachimvh@gmail.com>
2017-11-23 16:55:22 +0100

    Fix typings

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import { AsyncIterator, MultiTransformIterator, SimpleTransformIterator } from '
 
 export class DynamicNestedLoopJoin<L, R, T> extends MultiTransformIterator<L, T>
 {
-    constructor(left: AsyncIterator<L>, funRight: (L) => AsyncIterator<R>, funJoin: (L, R) => T);
+    constructor(left: AsyncIterator<L>, funRight: (left: L) => AsyncIterator<R>, funJoin: (left: L, right: R) => T);
 
     // problem due to async typings
     // protected _createTransformer(leftItem: L): SimpleTransformIterator<L, T>;
@@ -11,7 +11,7 @@ export class DynamicNestedLoopJoin<L, R, T> extends MultiTransformIterator<L, T>
 
 export class HashJoin<S, H, T> extends AsyncIterator<T>
 {
-    constructor(left: AsyncIterator<S>, right: AsyncIterator<S>, funHash: (S) => H, funJoin: (S, S) => T);
+    constructor(left: AsyncIterator<S>, right: AsyncIterator<S>, funHash: (entry: S) => H, funJoin: (left: S, right: S) => T);
     protected hasResults(): boolean;
     close(): void;
     read(): T;
@@ -19,7 +19,7 @@ export class HashJoin<S, H, T> extends AsyncIterator<T>
 
 export class NestedLoopJoin<L, R, T> extends MultiTransformIterator<L, T>
 {
-    constructor(left: AsyncIterator<L>, right: AsyncIterator<R>, funJoin: (L, R) => T);
+    constructor(left: AsyncIterator<L>, right: AsyncIterator<R>, funJoin: (left: L, right: R) => T);
     close(): void;
 
     // problem due to async typings
@@ -28,7 +28,7 @@ export class NestedLoopJoin<L, R, T> extends MultiTransformIterator<L, T>
 
 export class SymmetricHashJoin<S, H, T> extends AsyncIterator<T>
 {
-    constructor(left: AsyncIterator<S>, right: AsyncIterator<S>, funHash: (S) => H, funJoin: (S, S) => T);
+    constructor(left: AsyncIterator<S>, right: AsyncIterator<S>, funHash: (entry: S) => H, funJoin: (left: S, right: S) => T);
     protected hasResults(): boolean;
     close(): void;
     read(): T;
